aaa50be0748f7d95f28c6878d0aea03a3bb0ea5f
outan <outannexway@gmail.com>
2017-04-21 14:00:55 +0900

    chage 絶対path to 相対path in symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -4,7 +4,7 @@ for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
     then
-        ln -Fis "$PWD/$dotfile" "$HOME/$dotfile"
+        ln -Fis "dotfiles/$dotfile" "$HOME/$dotfile"
     fi
 done
 
